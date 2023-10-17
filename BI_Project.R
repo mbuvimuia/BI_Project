c2f121155beb0ff051881419e20726ab3655fc01
mbuvimuia <innocent.muia@strathmore.edu>
2023-10-17 16:49:58 +0300

    Update BI_Project.R

--- BI_Project.R
+++ BI_Project.R
@@ -78,21 +78,23 @@ vis_miss(loan_dataset) + theme(axis.text.x = element_text(angle = 80))
 gg_miss_upset(loan_dataset)
 
 
-#Imputation Process
-loan_daataset_numeric <- loan_dataset %>%
-  select(loan_amnt, funded_amnt, funded_amnt_inv, 
-         int_rate,installment, emp_title,emp_length,annual_inc,
-        dti, delinq_2yrs, inq_last_6mths, mths_since_last_delinq,
-        mths_since_last_record,open_acc, pub_rec, revol_bal, 
-        revol_util, total_acc, out_prncp_inv,total_pymnt, total_pymnt_inv,
-        total_rec_prncp, total_rec_int, total_rec_late_fee, recoveries, collection_recovery_fee
-        , last_pymnt_d, last_pymnt_amnt, last_credit_pull_d, collections_12_mths_ex_med,
-        mths_since_last_major_derog, policy_code, acc_now_delinq, tot_coll_amt,
-        tot_cur_bal, total_rev_hi_lim, )
+#Imputation Process 1
 
-md.pattern(loan_daataset_numeric)
+somewhat_correlated_variables <- quickpred(loan_dataset, mincor = 0.3)
+
+loan_dataset_mice <- mice(loan_dataset, m = 20, method = "pmm",
+                            seed = 7,
+                            predictorMatrix = somewhat_correlated_variables)
+
+loan_dataset_imputed <- mice::complete(loan_dataset_mice, 1)
 
 
+
+any_na(loan_dataset_imputed)
+
+n_miss(loan_dataset_imputed)
+
+loan_dataset_imputed %>% is.na() %>% colSums()
 
 
 ##STEP 2: Exploratory Data Analysis ----
