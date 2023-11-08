462c672916840baa760b704a65cffc8848647651
mbuvimuia <innocent.muia@strathmore.edu>
2023-11-08 13:02:02 +0300

    Creation of histograms

--- BI_Project.R
+++ BI_Project.R
@@ -177,6 +177,9 @@ any_na(loan_removed_vars_obs)
 
 miss_var_summary(loan_removed_vars_obs)
 
+#Remove unwanted columns
+df <- loan_removed_vars_obs[, -c(10, 14, 16, 17, 19, 20, 23, 43)]
+loan_data <- df
 
 ##STEP 4: Basic Exploratory Data Analysis ----
 ###Install the required packages ----
@@ -196,106 +199,95 @@ if (!is.element("e1071", installed.packages()[, 1])) {
 require("e1071")
 
 ###Previewing the dataset and Identify the datatypes ----
-dim(loan_removed_vars_obs)
-sapply(loan_removed_vars_obs, class)
+dim(loan_data)
+sapply(loan_data, class)
 
 #DESCRIPTIVE STATISTICS ----
 ###Identify the number of instances that belong to each class----
 #Count Categorical data
-loan_removed_vars_obs_purpose_freq <- loan_removed_vars_obs$purpose
-cbind(frequency = table(loan_removed_vars_obs_purpose_freq),
-      percentage = prop.table(table(loan_removed_vars_obs_purpose_freq)) * 100)
+loan_data_purpose_freq <- loan_data$purpose
+cbind(frequency = table(loan_data_purpose_freq),
+      percentage = prop.table(table(loan_data_purpose_freq)) * 100)
 
-loan_removed_vars_obs_loanstatus_freq <- loan_removed_vars_obs$loan_status
-cbind(frequency = table(loan_removed_vars_obs_loanstatus_freq),
-      percentage = prop.table(table(loan_removed_vars_obs_loanstatus_freq)) * 100)
+loan_data_loanstatus_freq <- loan_data$loan_status
+cbind(frequency = table(loan_data_loanstatus_freq),
+      percentage = prop.table(table(loan_data_loanstatus_freq)) * 100)
 
-loan_removed_vars_obs_verificationstatus_freq <- loan_removed_vars_obs$verification_status
-cbind(frequency = table(loan_removed_vars_obs_verificationstatus_freq),
-      percentage = prop.table(table(loan_removed_vars_obs_verificationstatus_freq)) * 100)
+loan_data_verificationstatus_freq <- loan_data$verification_status
+cbind(frequency = table(loan_data_verificationstatus_freq),
+      percentage = prop.table(table(loan_data_verificationstatus_freq)) * 100)
 
 
 #MEASURE OF CENTRAL TENDENCY ----
 ##Calculate the mode----
-loan_removed_vars_obs_purpose_mode <- names(table(loan_removed_vars_obs$purpose))[
-  which(table(loan_removed_vars_obs$purpose) == max(table(loan_removed_vars_obs$purpose)))
+loan_data_purpose_mode <- names(table(loan_data$purpose))[
+  which(table(loan_data$purpose) == max(table(loan_data$purpose)))
 ]
-print(loan_removed_vars_obs_purpose_mode)
+print(loan_data_purpose_mode)
 
 
-loan_removed_vars_obs_loanstatus_mode <- names(table(loan_removed_vars_obs$loan_status))[
-  which(table(loan_removed_vars_obs$loan_status) == max(table(loan_removed_vars_obs$loan_status)))
+loan_data_loanstatus_mode <- names(table(loan_data$loan_status))[
+  which(table(loan_data$loan_status) == max(table(loan_data$loan_status)))
 ]
-print(loan_removed_vars_obs_loanstatus_mode)
+print(loan_data_loanstatus_mode)
 
 
 
 # MEASURES OF Distribution/Dispersion/Spread/Scatter/Variability ----
 ##Measure of distribution for each variable----
-summary(loan_removed_vars_obs)
+summary(loan_data)
 
 ##Measure the standard deviation for each variable----
 
-sapply(loan_removed_vars_obs[, 1:47], sd)
+sapply(loan_data[, 1:47], sd)
 
 ## Measure the variance for each variable----
 
-sapply(loan_removed_vars_obs[, 1:47], var)
+sapply(loan_data[, 1:47], var)
 
 ## Measure the kurtosis of each variable----
-sapply(loan_removed_vars_obs [, c( -1,-2,-6,-9,-10,-11,
+sapply(loan_data [, c( -1,-2,-6,-9,-10,-11,
                                    -13,-14,-15,-16,-17,-18,-19,-20,
                                    -23, -30, -43)], kurtosis, type = 2)
 
 ##Measure the skewness of each variable----
-sapply(loan_removed_vars_obs [, c( -1,-2,-6,-9,-10,-11,
+sapply(loan_data [, c( -1,-2,-6,-9,-10,-11,
                                    -13,-14,-15,-16,-17,-18,-19,-20,
                                    -23, -30, -43)], skewness, type = 2)
 
 ## Measure the covariance between variables ----
 
-loan_data_cov <- cov(loan_removed_vars_obs[, c( -1,-2,-6,-9,-10,-11,
+loan_data_cov <- cov(loan_data[, c( -1,-2,-6,-9,-10,-11,
                                              -13,-14,-15,-16,-17,-18,-19,-20,
                                              -23, -30, -43)])
 View(loan_data_cov)
 
 ## Measure the correlation between variables ----
-loan_data_cor <- cor(loan_removed_vars_obs[c( -1,-2,-6,-9,-10,-11,
+loan_data_cor <- cor(loan_data[c( -1,-2,-6,-9,-10,-11,
                                               -13,-14,-15,-16,-17,-18,-19,-20,
                                               -23, -30, -43)])
 View(loan_data_cor)
 
 ## Create histograms for each numeric attribute
-# hist(loan_removed_vars_obs[, 8], main = names(loan_removed_vars_obs)[8])
-# hist(loan_removed_vars_obs[, 12], main = names(loan_removed_vars_obs)[12])
-# hist(loan_removed_vars_obs[, 21], main = names(loan_removed_vars_obs)[21])
-# hist(loan_removed_vars_obs[, 22], main = names(loan_removed_vars_obs)[22])
-# hist(loan_removed_vars_obs[, 24], main = names(loan_removed_vars_obs)[24])
-# hist(loan_removed_vars_obs[, 25], main = names(loan_removed_vars_obs)[25])
-# hist(loan_removed_vars_obs[, 26], main = names(loan_removed_vars_obs)[26])
-# hist(loan_removed_vars_obs[, 27], main = names(loan_removed_vars_obs)[27])
-# hist(loan_removed_vars_obs[, 28], main = names(loan_removed_vars_obs)[28])
-# hist(loan_removed_vars_obs[, 29], main = names(loan_removed_vars_obs)[29])
-# hist(loan_removed_vars_obs[, 31], main = names(loan_removed_vars_obs)[31])
-# hist(loan_removed_vars_obs[, 32], main = names(loan_removed_vars_obs)[32])
-# hist(loan_removed_vars_obs[, 33], main = names(loan_removed_vars_obs)[33])
-# hist(loan_removed_vars_obs[, 34], main = names(loan_removed_vars_obs)[34])
-# hist(loan_removed_vars_obs[, 35], main = names(loan_removed_vars_obs)[35])
-# hist(loan_removed_vars_obs[, 36], main = names(loan_removed_vars_obs)[36])
-# hist(loan_removed_vars_obs[, 37], main = names(loan_removed_vars_obs)[37])
-# hist(loan_removed_vars_obs[, 38], main = names(loan_removed_vars_obs)[38])
-# hist(loan_removed_vars_obs[, 39], main = names(loan_removed_vars_obs)[39])
-# hist(loan_removed_vars_obs[, 40], main = names(loan_removed_vars_obs)[40])
-# hist(loan_removed_vars_obs[, 41], main = names(loan_removed_vars_obs)[41])
-# hist(loan_removed_vars_obs[, 42], main = names(loan_removed_vars_obs)[42])
-# hist(loan_removed_vars_obs[, 44], main = names(loan_removed_vars_obs)[44])
-# hist(loan_removed_vars_obs[, 45], main = names(loan_removed_vars_obs)[45])
-# hist(loan_removed_vars_obs[, 46], main = names(loan_removed_vars_obs)[46])
-# hist(loan_removed_vars_obs[, 47], main = names(loan_removed_vars_obs)[47])
+pub_rec <- as.numeric(unlist(loan_data[, 20]))
+open_acc <- as.numeric(unlist(loan_data[, 19]))
+month_since_last_deliq <- as.numeric(unlist(loan_data[, 18]))
+collections <- as.numeric(unlist(loan_data[,34]))
+delinq_2yrs <- as.numeric(unlist(loan_data[,16]))
+total_payment <- as.numeric(unlist(loan_data[,26]))
+
+par(mfrow = c(1, 1))
+
+hist(pub_rec, main = names(loan_data)[20], xlim = c(0,100), ylim = c(0,30))
+hist(open_acc, main = names(loan_data)[19], xlim = c(0,100), ylim = c(0,30))
+hist(month_since_last_deliq, main = names(loan_data)[18], xlim = c(0,100), ylim = c(0,30))
+hist(collections, main = names(loan_data)[34], xlim = c(0,20), ylim = c(0,50))
+hist(delinq_2yrs, main = names(loan_data)[16], xlim = c(0,40), ylim = c(0,20))
+hist(total_payment, main = names(loan_data)[26], xlim = c(0,44000), ylim = c(0,20))
 
 
 #STEP 5: Data Transformation ----
 library(dplyr)
 
-numerical_col <- loan_removed_vars_obs %>% 
+numerical_col <- loan_data %>% 
   select_if(is.numeric)
